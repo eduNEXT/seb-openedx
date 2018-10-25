31df703ec4b4793af1e3c9c59a78b110cacf2885
Ivan Castellanos <ivanca@gmail.com>
2018-10-25 15:58:30 -0500

    Converting user_name to its own variable Step needed to support both next PRs user-banning and masquerade support

--- seb_openedx/middleware.py
+++ seb_openedx/middleware.py
@@ -24,7 +24,6 @@ class SecureExamBrowserMiddleware(MiddlewareMixin):
         """ Start point of to d4etermine cms or lms """
         course_key_string = view_kwargs.get('course_key_string') or view_kwargs.get('course_id')
         course_key = CourseKey.from_string(course_key_string) if course_key_string else None
-        access_denied = False
 
         if course_key:
             # By default is all denied
@@ -38,6 +37,9 @@ class SecureExamBrowserMiddleware(MiddlewareMixin):
                 # Second: Granular white-listing
                 access_denied = True
 
+            user_name = request.user.username if hasattr(request, 'user') else None
+            if hasattr(course_masquerade, 'user_name'):
+                user_name = course_masquerade.user_name
             active_comps = get_enabled_permission_classes()
             for permission in active_comps:
                 if permission().check(request, course_key):
