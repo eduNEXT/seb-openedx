52c4c7941ea572bc82a42dd54f49509c358dc584
Felipe Montoya <felipe.montoya@edunext.co>
2019-03-18 15:40:56 -0500

    Bump version: 0.8.2 → 0.8.3

--- seb_openedx/__init__.py
+++ seb_openedx/__init__.py
@@ -3,7 +3,7 @@ Init for main seb_openedx app
 """
 from __future__ import unicode_literals
 
-__version__ = '0.8.2'
+__version__ = '0.8.3'
 
 
 default_app_config = 'seb_openedx.apps.SebOpenEdxConfig'  # pylint: disable=invalid-name
