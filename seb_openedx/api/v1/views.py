0d50b12aca260dde44f25c69f520773358088ba2
Felipe Montoya <felipe.montoya@edunext.co>
2019-05-09 16:05:13 -0500

    Adding a serializer that supports the read operations of the API

--- seb_openedx/api/v1/views.py
+++ seb_openedx/api/v1/views.py
@@ -14,12 +14,16 @@ from rest_framework_oauth.authentication import OAuth2Authentication
 
 from opaque_keys.edx.keys import CourseKey
 
+from seb_openedx.api.v1.serializers import SebConfigurationSerializer
+from seb_openedx.seb_keys_sources import get_config_by_course
+
+
 LOG = logging.getLogger(__name__)
 
 
 class SebConfiguration(APIView):
     """
-    Handles API requests to create users
+    Handles all the CRUD operations on a Course SAFE_EXAM_BROWSER configuration
     """
 
     authentication_classes = (OAuth2Authentication, SessionAuthentication)
@@ -28,6 +32,11 @@ class SebConfiguration(APIView):
 
     def get(self, request, course_id, *args, **kwargs):
         """
+        Retrieves the current course configuration and validates it
+        before returning it to the caller
         """
         course_key = CourseKey.from_string(course_id)
-        return Response({"raw_data": unicode(course_key)})
+        config = get_config_by_course(course_key)
+        serialized_config = SebConfigurationSerializer(data=config)
+        serialized_config.is_valid(raise_exception=True)
+        return Response(serialized_config.validated_data)
