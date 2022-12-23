d3a89d882b6a7078246e4447a752946353cdb104
Deimer M <deimer.morales@edunext.co>
2022-12-23 13:29:20 -0500

    fix: Adding app_name
    Adding app_name to the URLs files

--- seb_openedx/dashboard/urls.py
+++ seb_openedx/dashboard/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from django.contrib.admin.views.decorators import staff_member_required
 from seb_openedx.dashboard.views import TableView
 
+app_name = 'seb-dashboard'
+
 urlpatterns = [  # pylint: disable=invalid-name
     url(r'^$', staff_member_required(TableView.as_view())),
 ]
