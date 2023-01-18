83226cea3e0005cb34d8e49da867409b29004de8
Cristhian Garcia <crisgarta8@gmail.com>
2023-01-18 13:54:05 -0500

    Bump version: 1.3.1 → 2.0.0

--- seb_openedx/__init__.py
+++ seb_openedx/__init__.py
@@ -3,7 +3,7 @@ Init for main seb_openedx app
 """
 from __future__ import unicode_literals
 
-__version__ = '1.3.1'
+__version__ = '2.0.0'
 
 
 default_app_config = 'seb_openedx.apps.SebOpenEdxConfig'  # pylint: disable=invalid-name
