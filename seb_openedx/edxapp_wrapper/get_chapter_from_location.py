e0e6ffb2d560cfcc36d59edf1d947bfbe25dde71
Felipe Montoya <felipe.montoya@edunext.co>
2024-05-07 15:41:05 -0500

    feat: refactor the connection to modulestore to retrieve any node from parent tree

--- seb_openedx/edxapp_wrapper/get_chapter_from_location.py
+++ seb_openedx/edxapp_wrapper/get_chapter_from_location.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 
 def get_chapter_from_location(*args, **kwargs):
-    """ Creates the edxapp user """
+    """ Finds the ID of the parent chapter in the xblock tree """
 
     backend_function = settings.SEB_GET_CHAPTER_FROM_LOCATION
     backend = import_module(backend_function)
