5d22a0cfd7dfa241f1724f9918361f7876cf5c62
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2024-05-17 16:14:33 -0500

    chore: bump version to 3.2.0 (#76)

--- seb_openedx/__init__.py
+++ seb_openedx/__init__.py
@@ -3,7 +3,7 @@ Init for main seb_openedx app
 """
 from __future__ import unicode_literals
 
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 
 
 default_app_config = 'seb_openedx.apps.SebOpenEdxConfig'  # pylint: disable=invalid-name
